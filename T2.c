66f4cf8a1519218e1ff566874796cb72eb2536f2
Gustavo-de-Oliveira <gustavo_de_oliveira@usp.br>
2018-04-18 16:19:04 -0300

    some_message

--- T2.c
+++ T2.c
@@ -10,34 +10,63 @@ int main(int argc, char const *argv[]){
 	scanf("%d %d %f", &n, &it, &t);
 
 	int tabela[n][it];
-	float similaridade[n][n-1];
+	float similaridade[n][n];
   
 	for (int i = 0; i < n; ++i){
 		for (int j = 0; j < it; ++j){
-			scanf("%d", &tabela[i][it]);
-		}
-	}
-   
-	for (int i = 0; i < n; ++i){
-		for (int j = 0; j < it; ++j){
-			printf(" %d ", tabela[i][it]);
+			tabela[i][j] = 0;
+			scanf("%d", &tabela[i][j]);
 		}
 	}
 	//fim bloco de leitura das variaveis e da tabela/matriz
 
-	/*//inicio do bloco para calculo das similaridades
-	int nominador;
-	float denominador;
+	//inicio do bloco para calculo das similaridades
+	int nominadorsimilaridade;
 	float raiz1, raiz2;
-	for (int i = 0; i < n; ++i){
-		nominador = 0; denominador = 0.0; raiz1 = 0; raiz2 = 0;
+	
+	nominadorsimilaridade = 0; raiz1 = 0; raiz2 = 0;	
+	for (int i = 0; i < n; i = ++i){
 		for (int j = 0; j < n; ++j){
-				for (int k = 0; k < it; ++k){
-					nominador += tabela[i][j]*tabela[i][k];
-					printf("%d\n", nominador);
+			for (int k = 0; k < it; ++k){
+				nominadorsimilaridade += tabela[i][k] * tabela[j][k];
+				raiz1 += pow(tabela[i][k], 2);
+				raiz2 += pow(tabela[j][k], 2);
 			}
+			similaridade[i][j] = nominadorsimilaridade/(sqrt(raiz1) * sqrt(raiz2));
+			if (similaridade[i][j] == 1.00){
+				similaridade[i][j] = 0;
+			}
+			nominadorsimilaridade = 0; raiz1 = 0; raiz2 = 0;
 		}
-	}*/
+	}
+	//fim do bloco de calculo de similaridade
 
+	//inicio do bloco para calculo de estimativa de nota
+	float nominadornota = 0.0;
+	float denominadornota = 0.0;
+
+	for (int i = 0; i < n; ++i){
+		for (int j = 0; j < it; ++j){
+			if(tabela[i][j] == 0){
+				for (int k = 0; k < n; ++k){
+					if (similaridade[i][k] >= t){
+						if (tabela[k][j] > 0){
+							nominadornota += similaridade[i][k] * tabela[k][j];
+							denominadornota += similaridade[i][k];
+						}
+					}
+				}
+				printf("%.2f\n", nominadornota/denominadornota);
+			}
+			denominadornota = 0.0;
+			nominadornota = 0.0;
+		}
+	}
 	return 0;
 }
+/*5 7 0.65	                                        
+5 3 2 3 4 4 4
+4 5 2 1 5 4 2
+1 4 4 2 3 5 1
+5 2 3 4 2 1 5
+1 0 2 3 2 3 0*/
